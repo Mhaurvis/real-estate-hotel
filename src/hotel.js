cc7d4f1622ec1e0f52408991ab4432c2860ca9bd
Mhaurvis <krizzymax12@gmail.com>
2023-10-24 08:24:51 +0100

    update correction

--- src/hotel.js
+++
@@ -1,8 +0,0 @@
-function handleScroll(){
-    const scrollY = window.scrollY || window.pageYOffset;
-
-    const faderoomDeals = document.getElementById("roomDeals");
-    fadingDiv.style.opacity = scrollY / 500;
-}
-
-window.addEventListener("scroll", handleScroll);
